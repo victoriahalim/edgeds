7c8aa10d9abdbe6c7a963e1000a9fa0b955bcf0d
victoriahalim <vhalim@deloitte.com.au>
2024-04-22 12:42:37 +1000

    try await header

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -107,7 +107,7 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  // loadFooter(doc.querySelector('footer'));
+  loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
